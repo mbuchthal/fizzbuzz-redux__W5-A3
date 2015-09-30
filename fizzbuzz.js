9311728a0a3e691e7c3638272718f1c960e6abbf
Mark Buchthal <mark.buchthal@gmail.com>
2015-09-30 14:45:09 -0700

    add refactor on event handler

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,4 +1,5 @@
 (function() {
+
 // object created
 var Fizzbuzz = (function() {
 
@@ -64,15 +65,16 @@ var Fizzbuzz = (function() {
 var fuzzBuzz = new Fizzbuzz();
 var btnFizzBuzz = document.getElementById("btn_fizzbuzz");
 
-//event listener that calls
-btnFizzBuzz.addEventListener("click", fizzHandler);
-
 function fizzHandler(e) {
   e.preventDefault();
   var start = document.getElementById("number_start").value;
   var end = document.getElementById("number_end").value;
   fuzzBuzz.read(start, end);
   fuzzBuzz.write(start, end);
-};
+}
+
+//event listener that calls
+btnFizzBuzz.addEventListener("click", fizzHandler);
+
 
 })();
