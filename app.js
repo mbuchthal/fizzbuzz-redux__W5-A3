95f751a6313ae99b45958f830935bffa347d4248
Mark Buchthal <mark.buchthal@gmail.com>
2015-10-03 10:06:27 -0700

    refactor app.js

--- app.js
+++ app.js
@@ -1,27 +1,57 @@
 
-// create new instance of fizzbuzz object
-var fuzzBuzz = new Fizzbuzz("fizz", "buzz");
+var FizzBuzz = require('sea-d44-fizz-buzz-mb');
+
+// set words and create new instance of fizzbuzz object
+var wordOne, wordTwo, result;
+var fuzzBuzz = new FizzBuzz(wordOne || "fizz", wordTwo || "buzz");
+
+// set vars for button events
 var btnFizzBuzz = document.getElementById("btn_fizzbuzz");
 var btnWords = document.getElementById("btn_fizz_word");
-var results = document.getElementById("results_fizzbuzz");
 
 //event function for fizzbuzz
 function fizzHandler(e) {
   e.preventDefault();
-  var start = document.getElementById("number_start").value;
-  var end = document.getElementById("number_end").value;
-  fuzzBuzz.read(start, end);
-  fuzzBuzz.write(start, end, results);
+  var startVal = document.getElementById("number_start").value;
+  var endVal = document.getElementById("number_end").value;
+  fuzzBuzz.input(parseInt(startVal), parseInt(endVal));
+  result = fuzzBuzz.output();
+  renderHeading(startVal, endVal);
+  renderList();
+}
+
+function renderList() {
+  var fuzzList = document.createElement("ol");
+  var elResults = document.getElementById("results_fizzbuzz");
+  while (elResults.hasChildNodes()) {
+    elResults.removeChild(elResults.firstChild);
+  }
+  for (var i = 0; i < result.length; i++ ) {
+    var fuzzListItem = document.createElement("li");
+    fuzzListItem.appendChild(document.createTextNode(result[i]));
+    fuzzList.appendChild(fuzzListItem);
+    elResults.appendChild(fuzzList);
+  }
+}
+
+//render heading displaying number start to end
+function renderHeading(first, second) {
+  var elFirstNum = document.getElementById("firstnum");
+  var elLastNum = document.getElementById("secondnum");
+  elFirstNum.textContent = "";
+  elLastNum.textContent = "";
+  elFirstNum.textContent = " -- " + first + " to ";
+  elLastNum.textContent = second;
 }
 
 //event function for word change form
 function wordHandler(e) {
   e.preventDefault();
-  var wordOne = document.getElementById("word_one").value;
-  var wordTwo = document.getElementById("word_two").value;
+  wordOne = document.getElementById("word_one").value;
+  wordTwo = document.getElementById("word_two").value;
   var elWordOne = document.getElementById("display_word_one");
   var elWordTwo = document.getElementById("display_word_two");
-  var fuzzBuzz = new Fizzbuzz(wordOne, wordTwo);
+  fuzzBuzz = new FizzBuzz(wordOne, wordTwo);
   elWordOne.textContent = "";
   elWordTwo.textContent = "";
   elWordOne.textContent = wordOne;
--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,67 +1,67 @@
-// object created
-// vars created here to be used in both _fizzbuzz and doFizzBuzz
-var Fizzbuzz = (function () {
-   var firstWord;
-   var secondWord;
+// // object created
+// // vars created here to be used in both _fizzbuzz and doFizzBuzz
+// var Fizzbuzz = (function () {
+//    var firstWord;
+//    var secondWord;
 
-// closure example
-  var _fizzbuzz = function(word1, word2) {
-    this.word1 = word1;
-    this.word2 = word2;
-    firstWord = this.word1;
-    secondWord = this.word2;
-  };
+// // closure example
+//   var _fizzbuzz = function(word1, word2) {
+//     this.word1 = word1;
+//     this.word2 = word2;
+//     firstWord = this.word1;
+//     secondWord = this.word2;
+//   };
 
-// the function where fizzbuzz happens
-  var doFizzBuzz = function(start, end) {
-    var answer = [];
+// // the function where fizzbuzz happens
+//   var doFizzBuzz = function(start, end) {
+//     var answer = [];
 
-    for (start; start <= end; start++) {
-      if ((start % 3 === 0) && (start % 5 === 0)) {
-        answer.push(firstWord + secondWord);
-      } else if (start % 3 === 0) {
-        answer.push(firstWord);
-      } else if (start % 5 === 0) {
-        answer.push(secondWord);
-      } else {
-        answer.push(start);
-      }
-    }
-    return answer;
-  };
+//     for (start; start <= end; start++) {
+//       if ((start % 3 === 0) && (start % 5 === 0)) {
+//         answer.push(firstWord + secondWord);
+//       } else if (start % 3 === 0) {
+//         answer.push(firstWord);
+//       } else if (start % 5 === 0) {
+//         answer.push(secondWord);
+//       } else {
+//         answer.push(start);
+//       }
+//     }
+//     return answer;
+//   };
 
-  _fizzbuzz.prototype = {
+//   _fizzbuzz.prototype = {
 
-// the read method to obtain numbers from click event and pass them to fizzbuzz
-    read: function(num1, num2) {
-      this.result = doFizzBuzz(num1, num2);
-    },
+// // the read method to obtain numbers from click event and pass them to fizzbuzz
+//     read: function(num1, num2) {
+//       this.result = doFizzBuzz(num1, num2);
+//     },
 
-// the Dom manipulation - both writes and removes text
-    write: function(start, end, results) {
-      var elFirstNum = document.getElementById("firstnum");
-      var elLastNum = document.getElementById("secondnum");
+// // the Dom manipulation - both writes and removes text
+//     write: function(start, end, results) {
+//       var elFirstNum = document.getElementById("firstnum");
+//       var elLastNum = document.getElementById("secondnum");
 
-// removes text from previous inquiries
-      while (results.hasChildNodes()) {
-        results.removeChild(results.firstChild);
-      }
-      elFirstNum.textContent = "";
-      elLastNum.textContent = "";
-      elFirstNum.textContent = " -- " + start + " to ";
-      elLastNum.textContent = end;
+// // removes text from previous inquiries
+//       while (results.hasChildNodes()) {
+//         results.removeChild(results.firstChild);
+//       }
+//       elFirstNum.textContent = "";
+//       elLastNum.textContent = "";
+//       elFirstNum.textContent = " -- " + start + " to ";
+//       elLastNum.textContent = end;
 
-// adds text
-      var fuzzList = document.createElement("ol");
-      for (var i = 0; i < this.result.length; i++ ) {
-        var fuzzListItem = document.createElement("li");
-        fuzzListItem.appendChild(document.createTextNode(this.result[i]));
-        fuzzList.appendChild(fuzzListItem);
-        results.appendChild(fuzzList);
-      }
-      return fuzzList;
-    }
-  };
+// // adds text
+//       var fuzzList = document.createElement("ol");
+//       for (var i = 0; i < this.result.length; i++ ) {
+//         var fuzzListItem = document.createElement("li");
+//         fuzzListItem.appendChild(document.createTextNode(this.result[i]));
+//         fuzzList.appendChild(fuzzListItem);
+//         results.appendChild(fuzzList);
+//       }
+//       return fuzzList;
+//     }
+//   };
 
-  return _fizzbuzz;
-})();
+//   return _fizzbuzz;
+// })();
